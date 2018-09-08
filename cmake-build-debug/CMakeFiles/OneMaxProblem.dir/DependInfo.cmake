fae82d9ec621a5ecccd7a1507a4b41a13cd42951
tunahan <tunahan@tunahan-pc.tunahan>
2018-09-08 13:54:00 +0300

    OO Style of Genetic Algorithms

--- cmake-build-debug/CMakeFiles/OneMaxProblem.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/OneMaxProblem.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/ctsnz0060/CLionProjects/OneMaxProblem/main.cpp" "/home/ctsnz0060/CLionProjects/OneMaxProblem/cmake-build-debug/CMakeFiles/OneMaxProblem.dir/main.cpp.o"
+  "/home/tunahan/CLionProjects/OneMaxOptimizationGenetic/main.cpp" "/home/tunahan/CLionProjects/OneMaxOptimizationGenetic/cmake-build-debug/CMakeFiles/OneMaxProblem.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- main.cpp
+++ main.cpp
@@ -52,13 +52,31 @@ public:
         std::cout << "  -> " << this->expected_number << std::endl;
     }
 
+    std::vector<Chromosome> evoluation(const Chromosome& chrom){
+        std::vector<Chromosome> chromosomes = std::vector<Chromosome>(2);
+
+        int num = rand() % GENE_SIZE;
+        for (int i = 0; i < GENE_SIZE; i++){
+            if (i <= num){
+                chromosomes[0].genes[i] = this->genes[i];
+                chromosomes[1].genes[i] = chrom.genes[i];
+            }
+            else{
+                chromosomes[0].genes[i] = chrom.genes[i];
+                chromosomes[1].genes[i] = this->genes[i];
+            }
+        }
+
+        return chromosomes;
+    }
+
+
     bool operator<(const Chromosome& A) const{
-        std::cout << expected_number << " - " << A.expected_number << std::endl;
         return std::round(expected_number) < std::round(A.expected_number);
     }
 
     friend std::ostream& operator<< (std::ostream& os, const Chromosome& obj) {
-        std::copy(std::begin(obj.genes), std::end(obj.genes), std::ostream_iterator<Chromosome>(os, ", "));
+        std::for_each(std::begin(obj.genes), std::end(obj.genes), [](int i){ std::cout << i << ", ";});
         return os;
     }
 
@@ -87,12 +105,9 @@ public:
 
     void calculateProbabilities(){
         std::for_each(std::begin(population_pool), std::end(population_pool),
-                      [=](Chromosome chro){ chro.calculateExpectedNumber(population_average);});
+                      [=](Chromosome &chro){ chro.calculateExpectedNumber(population_average);});
     }
 
-
-
-private:
     float getPopulationAverage(){
         int sum = 0;
         std::for_each(std::begin(population_pool), std::end(population_pool),
@@ -111,134 +126,101 @@ public:
         std::cout << population.population_average << std::endl;
         population.calculateProbabilities();
 
-        std::sort(std::begin(population.population_pool), std::end(population.population_pool));
+        std::sort(std::rbegin(population.population_pool), std::rend(population.population_pool));
 
-        std::copy(std::begin(population.population_pool), std::end(population.population_pool), std::ostream_iterator<Chromosome>(std::cout, ", "));
+        //print out for sorted list:
+        std::cout << "Sorted List: " << std::endl;
+        std::copy(std::begin(population.population_pool), std::end(population.population_pool), std::ostream_iterator<Chromosome>(std::cout, "\n"));
 
+        std::for_each(std::begin(population.population_pool), std::end(population.population_pool),
+                      [=](Chromosome chrom){
+                        if (std::round(chrom.expected_number) > 0){
+                            for (int i = 0; i < std::round(chrom.expected_number); i++){
+                                if (mate_pool.size() < population.population_size){
+                                    mate_pool.push_back(chrom);
+                                }
+                            }
+                        }
+                        else{
+                            for (int i = 0; i < std::round(chrom.expected_number) + 1; i++){
+                                if (mate_pool.size() < population.population_size){
+                                    mate_pool.push_back(chrom);
+                                }
+                            }
+                        }
+
+
+                      });
+
+        //print out for mate pool:
+        std::cout << "Mate Pool: (" << mate_pool.size() << ")" << std::endl;
+        std::copy(std::begin(mate_pool), std::end(mate_pool), std::ostream_iterator<Chromosome>(std::cout, "\n"));
+
+        population.population_pool = mate_pool;
+    }
+
+    std::pair<Chromosome, Chromosome> pickParentsUp(){
+
+        int rand1 = rand() % mate_pool.size();
+        int rand2 = rand() % mate_pool.size();
+
+        std::pair<Chromosome, Chromosome> result;
+        result.first = mate_pool[rand1];
+        result.second = mate_pool[rand2];
+
+        return result;
     }
 };
 
-std::vector<std::vector<int>> createAMatingPool(std::vector<std::vector<int>> pop, std::vector<int> actual_count){
-    std::vector<std::vector<int>> pool;
-    std::vector<std::vector<int>>::const_iterator it = pop.begin();
+class Generation{
+public:
+    std::vector<Population> generations;
 
-    std::for_each(std::begin(actual_count), std::end(actual_count),
-                  [&](int i) {
-                      if (i > 0) {
-                          for (int j = 0; j < i; ++j) {
-                              pool.push_back(*it);
-                          }
-                      }
-                      ++it;
-                  });
-
-    //DEBUG INFO
-    std::cout << "mating pool: ";
-    std::for_each(std::begin(pool), std::end(pool), [](std::vector<int> i){
-        std::for_each(std::begin(i), std::end(i), [](int j){
-            std::cout << j  << " "; } );
-    std::cout << std::endl;});
-    std::cout << "\n";
-
-    return  pool;
-
-}
-
-int calculateFitness(std::vector<int> chromosome){
-    int fitt_val = std::count(std::begin(chromosome), std::end(chromosome), 1);
-
-    std::cout << "fitness value: " << fitt_val << std::endl;
-
-    return fitt_val;
-}
-
-std::vector<int> calculateFitness(std::vector<std::vector<int>> pop){
-    std::vector<int> fitt_values(pop.size());
-
-    for (int i = 0; i < pop.size(); ++i) {
-        fitt_values[i] = calculateFitness(pop.at(i));
+    Generation(Population& initial_pop){
+        generations.push_back(initial_pop);
     }
 
-    return fitt_values;
-}
+    void evaluation(){
+        //pre evaluation:
+        Population the_next_generation;
+        Mater mating_operator = Mater(generations.back());
 
-void mutation(std::vector<int> &chromosome){
-    //DEBUG INFO
-    std::cout << "before mutation: ";
-    std::for_each(std::begin(chromosome), std::end(chromosome), [](int i){ std::cout << i << " "; });
-    std::cout << "\n";
+        the_next_generation.population_size = 0;
+        for (int i = 0; i < mating_operator.mate_pool.size()/2; i++) {
+            std::pair<Chromosome, Chromosome> parents = mating_operator.pickParentsUp();
+            std::vector<Chromosome> children = parents.first.evoluation(parents.second);
 
-    int x = rand() % (chromosome.size() - 1);
-    int num = rand() % 10 + 1;
-    chromosome.at(x) = num;
+            std::for_each(std::begin(children), std::end(children),
+                          [&the_next_generation](Chromosome chro){ the_next_generation.population_pool.push_back(chro);});
+            the_next_generation.population_size += 2;
+        }
 
-    //DEBUG INFO
-    std::cout << "after mutation: ";
-    std::for_each(std::begin(chromosome), std::end(chromosome), [](int i){ std::cout << i << " "; });
-    std::cout << "\n";
-}
-
-void startEvol(std::vector<std::vector<int>> pop, int termination_condition){
-    bool isTerminated = false;
-
-    while(!isTerminated){
-        std::vector<int> fitt_value_vec;
-        std::vector<int> actual_counts;
-        std::vector<float> expected_counts;
-        std::vector<std::vector<int>> mating_pool;
-
-        fitt_value_vec = calculateFitness(pop);
-        int sum = std::accumulate(std::begin(fitt_value_vec), std::end(fitt_value_vec), 0);
-        float average = std::roundf(((float)sum / pop.size())*100) / 100;
-
-        std::for_each(std::begin(fitt_value_vec), std::end(fitt_value_vec),
-                      [&expected_counts, average](int i){expected_counts.push_back(((float)i / average));});
-        //DEBUG INFO
-        std::cout << "expected counts: ";
-        std::for_each(std::begin(expected_counts), std::end(expected_counts), [](float i){ std::cout << i << " "; });
-        std::cout << "\n";
-
-        //DEBUG INFO
-        std::cout << "\nEXpected Count: " << std::accumulate(std::begin(expected_counts), std::end(expected_counts), 0.0f) << std::endl;
-
-
-        std::for_each(std::begin(fitt_value_vec), std::end(fitt_value_vec),
-                      [&actual_counts, average](int i){actual_counts.push_back(std::round((float)i / average));});
-        //DEBUG INFO
-        std::cout << "actual counts: ";
-        std::for_each(std::begin(actual_counts), std::end(actual_counts), [](int i){ std::cout << i << " "; });
-        std::cout << "\n";
-
-        //DEBUG INFO
-        std::cout << "\nActual Count: " << std::accumulate(std::begin(actual_counts), std::end(actual_counts), 0) << std::endl;
-
-        //create a mating pool
-        mating_pool = createAMatingPool(pop, actual_counts);
+        float average = the_next_generation.getPopulationAverage();
+        std::cout << average << std::endl;
+        generations.push_back(the_next_generation);
     }
-
-
-}
+};
 
 int main(){
-
     /* initialize random seed: */
     srand (time(NULL));
 
-/*
-    //initialize the first population.
-    int num, pop_size = 5, N = 5;
-    std::vector<std::vector<int>> population(pop_size, std::vector<int>(N));
-    for (int j = 0; j < pop_size; ++j) {
-        for (int i = 0; i < N; ++i) {
-            num = rand() % 10 + 1;
-            population[j][i] = num;
-        }
-    }
-
-    startEvol(population, N);
-*/
-
     Population pop = Population(100);
-    Mater mater = Mater(pop);
+    Generation gen = Generation(pop);
+    gen.evaluation();
+    gen.evaluation();
+    gen.evaluation();
+    gen.evaluation();
+    gen.evaluation();
+    gen.evaluation();
+    gen.evaluation();
+    gen.evaluation();
+    gen.evaluation();
+    gen.evaluation();
+    gen.evaluation();
+    gen.evaluation();
+    gen.evaluation();
 
+    std::for_each(std::begin(gen.generations), std::end(gen.generations), [](Population gen){ std::cout << "Average: " << gen.population_average << std::endl;});
+    //Mater mater = Mater(pop);
 };
