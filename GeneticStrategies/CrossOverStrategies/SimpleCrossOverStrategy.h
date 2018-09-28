f3ea3cbfb23aec0b90f30443254b17ff743c7c0d
tunahan <cataktunahan@gmail.com>
2018-09-28 18:41:08 +0300

    seperate inline functions for CrossOverStrategies folder

--- GeneticStrategies/CrossOverStrategies/SimpleCrossOverStrategy.h
+++ GeneticStrategies/CrossOverStrategies/SimpleCrossOverStrategy.h
@@ -5,39 +5,16 @@
 #ifndef ONEMAXPROBLEM_SIMPLECROSSOVERSTRATEGY_H
 #define ONEMAXPROBLEM_SIMPLECROSSOVERSTRATEGY_H
 
-    class SimpleCrossOverStrategy : public CrossOverStrategy {
+#include "../../GeneticCore/Chromosome.h"
+#include "../../GeneticCore/Population.h"
+#include "CrossOverStrategy.h"
 
-    public:
-        Population&& CreateChildren(std::vector<Chromosome> mating_pool){
-            Population children_for_next_gen;
-            for (int i = 0; i < mating_pool.size()/2; i++) {
-                std::vector<Chromosome> parents = PickParentsUp(mating_pool);
-                std::vector<Chromosome> children = PerformCrossOver(parents);
+class SimpleCrossOverStrategy : public CrossOverStrategy {
+public:
+    Population&& CreateChildren(std::vector<Chromosome>);
+    std::vector<Chromosome> PerformCrossOver(std::vector<Chromosome>) override;
 
-                std::copy(children.begin(), children.end(), std::back_inserter(children_for_next_gen.population_pool));
-                children_for_next_gen.population_size += 2; //TODO check if it is still needed
-            }
-
-            return std::move(children_for_next_gen);
-        }
-
-        virtual std::vector<Chromosome> PerformCrossOver(std::vector<Chromosome> parents) {
-            std::vector<Chromosome> chromosomes = std::vector<Chromosome>(2);
-            int num = rand() % 5; // TODO make it correct
-            for (int i = 0; i < 5; i++) { //TODO make it correct
-                if (i <= num) {
-                    chromosomes[0].genes[i] = parents[0].genes[i];
-                    chromosomes[1].genes[i] = parents[1].genes[i];
-                } else {
-                    chromosomes[0].genes[i] = parents[0].genes[i];
-                    chromosomes[1].genes[i] = parents[1].genes[i];
-                }
-            }
-
-            return chromosomes;
-        };
-
-    };
+};
 
 
 
